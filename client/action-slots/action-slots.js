ab573674832ec400cb185ba0f1cdb162d80c5d52
Paulo Ragonha <paulo@ragonha.me>
2015-11-07 14:37:55 +0100

    Add empty action and extra states to the action buttons

--- client/action-slots/action-slots.js
+++ client/action-slots/action-slots.js
@@ -3,13 +3,13 @@ import React from 'react'
 export default React.createClass({
   render: function () {
     return <ul className='action-slots'>
-      <li><button className='action-up'></button></li>
-      <li><button className='action-down'></button></li>
-      <li><button className='action-left'></button></li>
-      <li><button className='action-right'></button></li>
+      <li><button className='action-up past'></button></li>
+      <li><button className='action-down past'></button></li>
+      <li><button className='action-left current'></button></li>
+      <li><button className='action-right active'></button></li>
       <li><button className='action-beam'></button></li>
       <li><button className='action-blast'></button></li>
-      <li><button className='action-shield'></button></li>
+      <li><button className='action-empty'></button></li>
     </ul>
   }
 })
