a2cfa3b8b4fe57e949a573cd048f7e26b8bafc97
Paulo Ragonha <paulo@ragonha.me>
2015-11-07 14:51:32 +0100

    Extracts ActionButton component

--- client/action-slots/action-slots.js
+++ client/action-slots/action-slots.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import ActionButton from '../action-button/action-button'
 
 export default React.createClass({
   render: function () {
     return <ul className='action-slots'>
-      <li><button className='action-up past'></button></li>
-      <li><button className='action-down past'></button></li>
-      <li><button className='action-left current'></button></li>
-      <li><button className='action-right active'></button></li>
-      <li><button className='action-beam'></button></li>
-      <li><button className='action-blast'></button></li>
-      <li><button className='action-empty'></button></li>
+      <li><ActionButton action='up past' status='past'/></li>
+      <li><ActionButton action='down past' status='past'/></li>
+      <li><ActionButton action='left current' status='current'/></li>
+      <li><ActionButton action='right active' status='active'/></li>
+      <li><ActionButton action='beam'/></li>
+      <li><ActionButton action='blast'/></li>
+      <li><ActionButton action='empty'/></li>
     </ul>
   }
 })
