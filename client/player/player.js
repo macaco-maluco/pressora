25035215893b947ac12f81308e94fe4512382dc3
Paulo Ragonha <paulo@ragonha.me>
2015-11-07 22:53:15 +0100

    Fix lint errors

--- client/player/player.js
+++ client/player/player.js
@@ -9,10 +9,10 @@ export default React.createClass({
 
   render: function () {
     return <div className={`player player-1 player-${this.props.player.status} player-facing-${this.props.player.pos.facing}`}>
-      <audio id="beam-sound" src={player1Beam} preload="auto">
+      <audio id='beam-sound' src={player1Beam} preload='auto'>
         Your browser does not support the <code>audio</code> element.
       </audio>
-      <audio id="blast-sound" src={player1Blast} preload="auto">
+      <audio id='blast-sound' src={player1Blast} preload='auto'>
         Your browser does not support the <code>audio</code> element.
       </audio>
       <div className='flames'/>
