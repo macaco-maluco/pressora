e44cbd9e99c3dc49c9ad85824e92e888c16b8411
Paulo Ragonha <paulo@ragonha.me>
2015-11-07 10:40:51 +0100

    Webpack Hello world!

--- client/index.js
+++ client/index.js
@@ -1,1 +1,10 @@
-window.alert('yay')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var HelloMessage = React.createClass({
+  render: function () {
+    return <div>Hello World</div>
+  }
+})
+
+ReactDOM.render(<HelloMessage/>, document.getElementById('main'))
