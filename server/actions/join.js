8549e7183dec73a902ae8f4bd9c7fee0b46a33a4
Marcio Oliveira <marcio.oliveira@klarna.com>
2015-11-07 14:37:41 +0100

    Adding start-match and wait-for-players events

--- server/actions/join.js
+++ server/actions/join.js
@@ -18,7 +18,9 @@ module.exports = function (io) {
   io.on('connection', function (socket) {
     var session = socket.handshake.session
     var match = findMatch(session.matchId)
+    socket.join(match.id)
     if (match.isReadyToStart()) {
+      socket.to(match.id).emit('start-match')
       socket.emit('start-match')
     } else {
       socket.emit('wait-for-players')
