1f01a6f86fd5a48b2465e97d76ddc39a4710f264
Marcio Oliveira <marcio.oliveira@klarna.com>
2015-11-07 19:50:50 +0100

    Backward action

---
+++ server/commands/backward.js
@@ -0,0 +1,9 @@
+var Directions = require('../domain/directions')
+
+module.exports = function(match, player) {
+  var originalDirection = player.pos.facing.slice(0)
+  var oppositeDirection = Directions[Directions[player.pos.facing].left].left
+  player.pos.facing = oppositeDirection
+  require('./forward')(match, player)
+  player.pos.facing = originalDirection
+}
