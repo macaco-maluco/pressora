3c2a8541997540fb45634c0b723a2e22dca9f2b4
Paulo Ragonha <paulo@ragonha.me>
2015-11-08 22:58:48 +0100

    Changes create game to POST to avoid cache :eyes:

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,5 @@
 module.exports = function (app, io) {
-  app.get('/api/game', require('./actions/game'))
+  app.post('/api/game', require('./actions/game'))
   app.get('/api/stats', require('./actions/stats'))
   require('./actions/join')(io)
 }
