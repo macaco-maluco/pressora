753eaea6e46d611ea4219db6977e422d67e3c67b
Túlio Ornelas <tulio.ornelas@klarna.com>
2015-11-08 20:12:23 +0100

    killing disconnected players

--- server/events/disconnect.js
+++ server/events/disconnect.js
@@ -1,5 +1,8 @@
 module.exports = function (context, socket) {
   return function () {
-    console.log(`player ${context.session.playerId} disconnected`)
+    var playerId = context.session.playerId
+    console.log(`player ${playerId} disconnected`)
+    var player = context.match.players.find((p) => p.id === playerId)
+    if (player) player.die('disconnected')
   }
 }
